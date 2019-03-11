19547fb7cc04daf842cb8dfe17a039db6998fad7
Omer Buyukguclu <omerbguclu@gmail.com>
2019-03-11 19:45:57 +0200

    11.03.2019 pazartesi dersi

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './post/post.component';
+import { UserComponent } from './user/user.component';
+import { NewUserComponent } from './user/new-user/new-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"post",
+    component:PostComponent
+  },
+  {
+    path:"user",
+    component:UserComponent
+  },
+  {
+    path:"",
+    component:NewUserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
